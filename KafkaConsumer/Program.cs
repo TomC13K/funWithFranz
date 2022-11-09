d0c6a1d9adf03ae4e72e64c8d0c7b6b9aac0293c
Tom <tomas.ciernik@and.digital>
2022-11-09 08:27:25 +0000

    consumer files

---
+++ KafkaConsumer/Program.cs
@@ -0,0 +1,38 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+
+Console.WriteLine("Franz - the consumer!");
+
+var config = new ConsumerConfig
+{
+    GroupId = "weather-consumer-group",
+    BootstrapServers = "localhost:9092",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+var consumer = new ConsumerBuilder<Null, string>(config).Build();
+
+consumer.Subscribe("kafka-topic");
+
+CancellationTokenSource token = new();
+
+try
+{
+    while (true)
+    {
+        var response = consumer.Consume(token.Token);
+        if (response.Message != null)
+        {
+            var weather = JsonConvert.DeserializeObject<Weather>(response.Message.Value);
+            Console.WriteLine($"State: {weather?.State} Temp: {weather?.Temperature} C");
+        }
+    }
+
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+    throw;
+}
+
+public record Weather(string State, int Temperature);
